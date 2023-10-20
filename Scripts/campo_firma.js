9c658977995f70abeabf417e2cbe25bc5098454a
tonyjg <tonyjgdd@gmail.com>
2023-10-19 20:05:05 -0500

    cambios 2.8

--- Scripts/campo_firma.js
+++ Scripts/campo_firma.js
@@ -1,11 +1,33 @@
-  // JavaScript para establecer la fecha actual en el campo de fecha
-  const fechaActual = new Date().toISOString().split('T')[0];
-  document.getElementById('fecha-nacimiento').value = fechaActual;
-
-  var canvas = document.getElementById('campo-firma');
-  var signaturePad = new SignaturePad(canvas, {
-      backgroundColor: 'white',
-      penColor: 'black',
-  });
 
   
+
+var canvas = document.getElementById('campo-firma');
+var signaturePad = new SignaturePad(canvas, {
+    backgroundColor: 'transparent', // Fondo transparente
+    penColor: 'blue',
+});
+
+// Configura la imagen de fondo
+var backgroundImage = new Image();
+backgroundImage.src = 'img/campo.png' // Cambia esto a la ruta correcta de tu imagen de fondo
+backgroundImage.onload = function () {
+    canvas.getContext('2d').drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+};
+
+var botonBorrar = document.getElementById('borrarCanvas');
+botonBorrar.addEventListener('click', function () {
+    signaturePad.clear();
+    canvas.getContext('2d').drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+});
+
+
+function cargarImagen(input, idVistaPrevia) {
+    const vistaPrevia = document.getElementById(idVistaPrevia);
+    if (input.files && input.files[0]) {
+        const lector = new FileReader();
+        lector.onload = function (e) {
+            vistaPrevia.src = e.target.result;
+        };
+        lector.readAsDataURL(input.files[0]);
+    }
+}
